f35246f7754083a5366ca4597708fe113c95ff6c
meher12 <meher.khemiri@gmail.com>
2022-05-05 18:56:41 +0200

    update README file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactModule } from './modules/general/contact/contact.module';
+import { HomeComponent } from './modules/general/home/home.component';
+import { ContactComponent } from './modules/general/contact/contact.component';
+import { AboutComponent } from './modules/general/about/about.component';
+import { LoginComponent } from './modules/general/login/login.component';
+import { SignupComponent } from './modules/general/signup/signup.component';
+import { NotFoundComponent } from './modules/general/not-found/not-found.component';
+import { LoginModule } from './modules/general/login/login.module';
+import { SignupModule } from './modules/general/signup/signup.module';
+import { AboutModule } from './modules/general/about/about.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
